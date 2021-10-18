4c835d2d995ced2401c5a44850b4fd4f79e3c2e7
Marcelo Arrarte <32116762+MarceloArrarte@users.noreply.github.com>
2021-10-18 20:39:31 -0300

    Parte 1

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,6 +8,19 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+            PictureProvider provider = new PictureProvider();
+            IPicture picture = provider.GetPicture(@"beer.jpg");
+            
+            IPipe fin = new PipeNull();
+            IFilter negativo = new FilterNegative();
+            IPipe pipeNegativo = new PipeSerial(negativo, fin);
+
+            IFilter greyscale = new FilterGreyscale();
+            IPipe inicio = new PipeSerial(greyscale, pipeNegativo);
+
+            IPicture resultado = inicio.Send(picture);
+
+            provider.SavePicture(resultado, @"processed_image.jpg");
         }
     }
 }
